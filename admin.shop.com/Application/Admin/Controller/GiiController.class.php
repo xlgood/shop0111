3449be8cef09d1fd5af167045c35a1cf880e1152
River <1216415742@qq.com>
2016-01-12 07:51:02 +0800

    添加Gii控制器

---
+++ admin.shop.com/Application/Admin/Controller/GiiController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/1/11
+ * Time: 23:47
+ */
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class GiiController extends Controller{
+
+}
